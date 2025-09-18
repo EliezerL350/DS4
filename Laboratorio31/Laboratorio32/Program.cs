e1de1d6ce964f5c2e9d6779b5e085459e9e24c21
EliezerL <eliezer.lopez@utp.ac.pa>
2025-09-18 10:09:43 -0500

    Laboratorio3 culminado

---
+++ Laboratorio31/Laboratorio32/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Laboratorio32
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CalculosMatematicos calculosMatematicos = new CalculosMatematicos();
+
+            float radio;
+
+            Console.Write("Introduce el primer numero: ");
+            radio = Convert.ToInt32(Console.ReadLine());
+
+
+            float calcularOperacion = calculosMatematicos.calcularAreaCirculo(radio);
+            Console.WriteLine("Resultado {0}", calcularOperacion);
+        }
+    }
+    public class CalculosMatematicos
+    {
+        //Metodo de calculo para la operacion
+        public float calcularAreaCirculo(float radio)
+        {
+            float resultado = (radio + radio) * 3.141516f;
+            return resultado;
+        }
+    }
+
+}
