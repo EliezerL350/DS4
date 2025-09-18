b7f26accbe9733eebcd06106166564f9336b6922
EliezerL <eliezer.lopez@utp.ac.pa>
2025-09-17 22:51:32 -0500

    Avance del laboratorio3

---
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Laboratorio31
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CalculosMatematicos calculosMatematicos = new CalculosMatematicos();
+
+            int primerNumero, segundoNumero;
+
+            Console.Write("Introduce el primer numero: ");
+            primerNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduce el segundo numero: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Resultado {0}", calculosMatematicos.calcular);
+        }
+    }
+    public class CalculosMatematicos
+    {
+        int primerNumero, segundoNumero, calcular;
+
+        //Metodo de calculo
+        calcular = (primerNumero + segundoNumero) * (primerNumero - segundoNumero);
+    }
+
+}
