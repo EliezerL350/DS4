790a4e7f9a38cd5ddd5754ad94e252e7741399e4
EliezerL <eliezer.lopez@utp.ac.pa>
2025-09-18 19:35:49 -0500

    Laboratorio4 con 7 proyectos, 1 solucion

---
+++ Laboratorio4/Laboratorio41/Program.cs
@@ -0,0 +1,17 @@
+﻿static void Main (string[] args)
+{
+    int n, x = 0;
+    string linea;
+
+    Console.WriteLine("Ingrese el valor de n:");
+    linea = Console.ReadLine();
+    
+    n = 5;
+    while (x <= n)
+    {
+        Console.WriteLine(x);
+        Console.WriteLine(" , ");
+        x++;
+    }
+    Console.ReadKey();
+}
