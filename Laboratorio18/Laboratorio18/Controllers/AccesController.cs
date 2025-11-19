590c411e05ec51f50f84a98a4bd9fe54ac0d461c
EliezerL <eliezer.lopez@utp.ac.pa>
2025-11-18 23:07:24 -0500

    Laboratorio#8, Patron de diseño MCV

---
+++ Laboratorio18/Laboratorio18/Controllers/AccesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio18.Controllers
+{
+    public class AccesController : Controller
+    {
+        // GET: Acces
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(String user, string password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex) {
+                return Content("Ocurrio un error :(" + ex.Message);
+            }
+        }
+    }
+}
